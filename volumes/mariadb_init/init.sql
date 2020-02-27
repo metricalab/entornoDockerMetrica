e5d5f245c0fc49ea57aa56bc145a33d3ed67735c
Juan Pardo <juan.pardo@metrica.es>
2020-02-27 17:04:57 +0100

    mysql database

--- volumes/mariadb_init/init.sql
+++ volumes/mariadb_init/init.sql
@@ -1,10 +1,12 @@
 -- Crear la base de datos db_tasks
 CREATE DATABASE IF NOT EXISTS db_tasks;
+CREATE DATABASE IF NOT EXISTS db_clientes_ex_crud;
 
 -- Crear usuario
 CREATE USER 'tasksuser'@'%' IDENTIFIED BY 'taskspassword';
 
 -- Permisos para el nuevo usuario
 GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_tasks`.* TO 'tasksuser'@'%';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_clientes_ex_crud`.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
