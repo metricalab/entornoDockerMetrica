f1bb1cc80f477dfb01b144aba8907e77c1fa61ee
Juan Pardo <juan.pardo@metrica.es>
2020-03-02 13:06:58 +0100

    renamed to mysql

--- volumes/mysql_init/init.sql
+++ volumes/mysql_init/init.sql
@@ -1,6 +1,7 @@
 -- Crear la base de datos db_tasks
 CREATE DATABASE IF NOT EXISTS db_tasks;
 CREATE DATABASE IF NOT EXISTS db_clientes_ex_crud;
+CREATE DATABASE IF NOT EXISTS db_im_samlConnect;
 
 -- Crear usuario
 CREATE USER 'tasksuser'@'%' IDENTIFIED BY 'taskspassword';
@@ -8,5 +9,8 @@ CREATE USER 'tasksuser'@'%' IDENTIFIED BY 'taskspassword';
 -- Permisos para el nuevo usuario
 GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_tasks`.* TO 'tasksuser'@'%';
 GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_clientes_ex_crud`.* TO 'root'@'%';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_im_samlConnect`.* TO 'root'@'%';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_im_users`.* TO 'root'@'%';
+
 
 FLUSH PRIVILEGES;
