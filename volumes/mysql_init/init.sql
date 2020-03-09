c48de414c4ae0b8c135a5aa980a27739a5d76bb2
Juan Pardo <juan.pardo@metrica.es>
2020-03-09 15:37:09 +0100

    db added

--- volumes/mysql_init/init.sql
+++ volumes/mysql_init/init.sql
@@ -1,7 +1,7 @@
 -- Crear la base de datos db_tasks
 CREATE DATABASE IF NOT EXISTS db_tasks;
 CREATE DATABASE IF NOT EXISTS db_clientes_ex_crud;
-CREATE DATABASE IF NOT EXISTS db_im_samlConnect;
+CREATE DATABASE IF NOT EXISTS db_um_producto;
 
 -- Crear usuario
 CREATE USER 'tasksuser'@'%' IDENTIFIED BY 'taskspassword';
@@ -9,8 +9,7 @@ CREATE USER 'tasksuser'@'%' IDENTIFIED BY 'taskspassword';
 -- Permisos para el nuevo usuario
 GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_tasks`.* TO 'tasksuser'@'%';
 GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_clientes_ex_crud`.* TO 'root'@'%';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_im_samlConnect`.* TO 'root'@'%';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_im_users`.* TO 'root'@'%';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_um_producto`.* TO 'root'@'%';
 
 
 FLUSH PRIVILEGES;
